2656cd2aac16676a29ee3db0d8edf430a3a88aab
Marianela <marianelgau@gmail.com>
2022-02-21 18:28:17 -0300

    Commented tests

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,28 +1,28 @@
 # conftest.py
 import pytest
 from rest_framework.test import APIClient
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 @pytest.fixture(scope="session", autouse=True)
 def get_client():
     return APIClient()
 
-@pytest.fixture(scope="session", autouse=True)
-def create_user():
-	return User.objects.create_superuser('admin', 'myemail@test.com', 'admin')
+# @pytest.fixture(scope="session", autouse=True)
+# def create_user():
+# 	return User.objects.create_superuser('admin', 'myemail@test.com', 'admin')
 
-@pytest.fixture(scope="session", autouse=True)
-def get_token():
-   response = APIClient().post(
-        "/api/token/",
-        {
-            "username": "admin",
-            "password": "admin"
-        },
-        format="json"
-    )
-   return response.data['access']
+# @pytest.fixture(scope="session", autouse=True)
+# def get_token():
+#    response = APIClient().post(
+#         "/api/token/",
+#         {
+#             "username": "admin",
+#             "password": "admin"
+#         },
+#         format="json"
+#     )
+#    return response.data['access']
 
-@pytest.fixture(scope="session", autouse=True)
-def product_data():
-   return {"name":"producto 1", "price":360, "stock":5}
+# @pytest.fixture(scope="session", autouse=True)
+# def product_data():
+#    return {"name":"producto 1", "price":360, "stock":5}
